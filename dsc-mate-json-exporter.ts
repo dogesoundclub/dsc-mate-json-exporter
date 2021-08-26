8fa82d413e3c8c3335486c0f867850fc68424bcc
Hanul <hanul@hanul.me>
2021-08-26 23:01:51 +0900

    색상 정보 수정

--- dsc-mate-json-exporter.ts
+++ dsc-mate-json-exporter.ts
@@ -14,7 +14,7 @@ for (const token of database.collection) {
         name: token.name,
         description: "클레이튼 체인 최초의 제너레이티브 아트, 도지사운드클럽의 투표권으로 사용됨.",
         image: `https://storage.googleapis.com/dsc-mate/336/dscMate-${token.tokenId}.png`,
-        background_color: "#F8D602",
+        background_color: "#F6D613",
         sendable: true,
         attributes,
     }));
