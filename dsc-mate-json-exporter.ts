35bb0d945aa3a63d6ad61e49d21d6b7b3c19163b
Hanul <hanul@hanul.me>
2021-08-26 22:55:28 +0900

    dev json exporter

---
+++ dsc-mate-json-exporter.ts
@@ -0,0 +1,22 @@
+import database from "./database.json";
+import fs from "fs";
+
+for (const token of database.collection) {
+    const attributes: { trait_type: string, value: any, max_value?: number }[] = [{
+        trait_type: "Level",
+        value: token.levels.Attributes,
+        max_value: 11,
+    }];
+    for (const [trait_type, value] of Object.entries(token.properties)) {
+        attributes.push({ trait_type, value });
+    }
+    fs.writeFileSync(`./json/${token.tokenId}.json`, JSON.stringify({
+        name: token.name,
+        description: "클레이튼 체인 최초의 제너레이티브 아트, 도지사운드클럽의 투표권으로 사용됨.",
+        image: `https://storage.googleapis.com/dsc-mate/336/dscMate-${token.tokenId}.png`,
+        background_color: "#F8D602",
+        sendable: true,
+        attributes,
+    }));
+    console.log(`Mate #${token.tokenId} Uploaded.`);
+}
